6c00b6e77d2975d8b15d4b1a8a4c38a4b6d918c6
LieuTuong <34327140+LieuTuong@users.noreply.github.com>
2019-11-27 11:09:19 +0700

    Update hook_write.c

--- phan_2/hook_write.c
+++ phan_2/hook_write.c
@@ -27,36 +27,23 @@ int make_ro(unsigned long address){
 }
 
 
-void fd_to_pathname(int fd,char file_name[255])
-{
-	struct file *file;
-	int fput_needed;
-	
-	mm_segment_t segment;
-	
-	file = fget_light(fd, &fput_needed);
-	if (file)
-	{
-		segment = get_fs();
-		set_fs(KERNEL_DS);
-		file_name = file->f_path.dentry->d_iname;		
-		set_fs(segment);
-	}
-	fput_light(file, fput_needed);
-}
+
 
 
 
 asmlinkage ssize_t hook_write(int fd, const void *buf, size_t cnt)
 {
+	char *tmp;
 	char *pathname;
 	struct file *file;
 	int fput_needed;
 	mm_segment_t segment;
-
+	int  written_bytes ;
+	struct path *path;
+	
 	printk(KERN_INFO"This is my hook_write()\n");
 
-	file = fget_light(fd, &fput_needed);
+	/*file = fget_light(fd, &fput_needed);
 	if (file)
 	{
 		segment = get_fs();
@@ -64,18 +51,47 @@ asmlinkage ssize_t hook_write(int fd, const void *buf, size_t cnt)
 		pathname = file->f_path.dentry->d_iname;		
 		set_fs(segment);
 	}
-	fput_light(file, fput_needed);
+	fput_light(file, fput_needed);*/
+	spin_lock(&current->files->file_lock);
+file = fcheck_files(current->files, fd);
+if (!file) {
+    spin_unlock(&current->files->file_lock);
+    return -ENOENT;
+}
+
+path = &file->f_path;
+path_get(path);
+spin_unlock(&current->files->file_lock);
+
+tmp = (char *)__get_free_page(GFP_KERNEL);
+
+if (!tmp) {
+    path_put(path);
+    return -ENOMEM;
+}
+
+pathname = d_path(path, tmp, PAGE_SIZE);
+path_put(path);
+
+if (IS_ERR(pathname)) {
+    free_page((unsigned long)tmp);
+    return PTR_ERR(pathname);
+}
+
+/* do something here with pathname */
+
+free_page((unsigned long)tmp);
 
 	printk(KERN_INFO "Written file: %s\n", pathname);
 
-	int written_bytes = original_write(fd, buf, cnt);
+	written_bytes = original_write(fd, buf, cnt);
 
 	printk(KERN_INFO"Number of written bytes: %d\n",written_bytes);
 
 	return written_bytes;
 }
 
-static int __int init_mysyscall(void)
+static int __init init_mysyscall(void)
 {
 	printk(KERN_INFO "loaded mysyscall hook\n");
 	
